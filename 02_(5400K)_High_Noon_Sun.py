bc0c3478887c450e9c975ad34ad261c04e89ea9c
ctfynijuo6tf <129854977+ctfynijuo6tf@users.noreply.github.com>
2023-04-04 11:50:25 +0000

    Update 02_(5400K)_High_Noon_Sun.py

--- 02_(5400K)_High_Noon_Sun.py
+++ 02_(5400K)_High_Noon_Sun.py
@@ -17,8 +17,12 @@ lampdata.size = 30000000#0.02
 
 
 #lampdata.size_y = 0.02
+
+
 lampdata.shadow_ray_samples_x = 2
 #lampdata.shadow_ray_samples_y = 3
+
+
 lampdata.color = (1.0, 1.0, 1.0)
 lampdata.energy = 1.094316#91193 #lux
 lampdata.distance =695699968
